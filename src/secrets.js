25346015cfee3df21d4d659a30613d533dc58f13
Timothy Warner <timothywarner316@gmail.com>
2025-07-08 09:39:22 -0500

    Provoking a secrwet scanning laert

--- src/secrets.js
+++ src/secrets.js
@@ -1,13 +1,14 @@
-
 // DEMO SECRETS - FOR GHAS DEMO PURPOSES ONLY
 // These are fake secrets that will trigger Secret Scanning
 
 const STRIPE_API_KEY = 'sk_test_51ABC123DEF456GHI789JKL012MNO345PQR678STU901VWX234YZA567BCD890EFG';
+const fakeStripeApiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
 const AWS_ACCESS_KEY = 'AKIAIOSFODNN7EXAMPLE';
 const GITHUB_TOKEN = 'ghp_1234567890abcdef1234567890abcdef12345678';
 
 module.exports = {
   STRIPE_API_KEY,
+  fakeStripeApiKey,
   AWS_ACCESS_KEY,
   GITHUB_TOKEN
 };
